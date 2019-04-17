4793ebee7c7b5ec9e3bad3a94e3ba2ddbeed3ef0
Alex Zherdev <alex_zherdev@acl.com>
2019-04-17 09:57:45 -0700

    Only support webpack >=4

--- index.js
+++ index.js
@@ -11,43 +11,27 @@ Object.assign(module.exports, __export__);`;
 		compilation.assets[filename] = new ConcatSource(compilation.assets[filename], source);
 	}
 
-	_handleOptimizeChunkAssets(chunks, cb, compilation) {
-		const options = compilation.outputOptions;
-
-		for (const chunk of chunks) {
-			for (const filename of chunk.files) {
-				if (filename === options.filename) {
-					this._add(compilation, filename);
-					break;
-				}
-			}
-		}
-
-		cb();
-	}
-
-	_handleCompilation(compilation) {
-		const options = compilation.outputOptions;
-
-		if (options.libraryTarget !== 'commonjs2') {
-			compilation.errors.push(new Error('AddModuleExportsPlugin: output.libraryTarget must be `commonjs2`'));
-			return;
-		}
-
-		if (compilation.hooks) {
-			compilation.hooks.optimizeChunkAssets.tapAsync('AddModuleExportsPlugin',
-				(chunks, cb) => this._handleOptimizeChunkAssets(chunks, cb, compilation));
-		} else {
-			compilation.plugin('optimize-chunk-assets',
-				(chunks, cb) => this._handleOptimizeChunkAssets(chunks, cb, compilation));
-		}
-	}
-
 	apply(compiler) {
-		if (compiler.hooks) {
-			compiler.hooks.compilation.tap('AddModuleExportsPlugin', this._handleCompilation.bind(this));
-		} else {
-			compiler.plugin('compilation', this._handleCompilation.bind(this));
-		}
+		compiler.hooks.compilation.tap('AddModuleExportsPlugin', compilation => {
+			const options = compilation.outputOptions;
+
+			if (options.libraryTarget !== 'commonjs2') {
+				compilation.errors.push(new Error('AddModuleExportsPlugin: output.libraryTarget must be `commonjs2`'));
+				return;
+			}
+
+			compilation.hooks.optimizeChunkAssets.tapAsync('AddModuleExportsPlugin', (chunks, cb) => {
+				for (const chunk of chunks) {
+					for (const filename of chunk.files) {
+						if (filename === options.filename) {
+							this._add(compilation, filename);
+							break;
+						}
+					}
+				}
+
+				cb();
+			});
+		});
 	}
 };
